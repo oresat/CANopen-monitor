dbea9487b3a9b657e097bea8c63b01046d73e7fc
Brian ONeill <Boneill3@users.noreply.github.com>
2020-09-07 17:15:56 -0700

    Adding heartbeat parser

--- canopen_monitor/parser/hb.py
+++ canopen_monitor/parser/hb.py
@@ -1,2 +1,20 @@
+"""
+Parse Heartbeat message
+
+@:param: data: a byte string containing the heartbeat message
+@:returns: string describing the message
+"""
+
+
 def parse(data: bytes):
-    return 'Heartbeat!'
+    states = {
+        0x00: "Boot-up",
+        0x04: "Stopped",
+        0x05: "Operational",
+        0x7F: "Pre-operational"
+    }
+
+    if int.from_bytes(data, "big") in states:
+        return f"Heartbeat - {states[int.from_bytes(data, 'big')]}"
+    else:
+        raise ValueError("Invalid heartbeat state detected")
