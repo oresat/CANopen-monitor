06999847a6e525bb347f8d9eeffb3fc727257496
Brian ONeill <Boneill3@users.noreply.github.com>
2021-01-03 19:34:47 -0800

    Pane cleanup and documentation

--- canopen_monitor/canmsgs/canmsg_table.py
+++ canopen_monitor/canmsgs/canmsg_table.py
@@ -1,18 +1,44 @@
+from __future__ import annotations
 from .canmsg import CANMsg
 import typing
+from collections.abc import Iterable, Iterator
 
 
-class CANMsgTable:
-    def __init__(self, capacity: int = None):
+class CANMsgTable(Iterable):
+    """
+    Table of CAN Messages
+    """
+    def __init__(self: CANMsgTable, capacity: int = None):
         self.__message_table = {}
-        self.__capacity = capacity
+        self.capacity = capacity
 
-    def __sort__(self) -> [int]:
+    def __sort__(self: CANMsgTable) -> [int]:
+        """
+        Overloaded sort function
+        Sort by COB-ID
+
+        Returns
+        --------
+        [int]: List of keys sorted
+        """
         return sorted(self.__message_table.keys())
 
-    def __add__(self, frame: CANMsg) -> None:
-        if self.__capacity is not None:
-            if (len(self.__message_table) < self.__capacity
+    def __add__(self: CANMsgTable, frame: CANMsg) -> CANMsgTable:
+        """
+        Overloaded add operator
+        allows for following:
+        CANMsgTable += CANMsg
+
+        Arguments
+        ----------
+        frame: CANMsg to be added
+
+        Returns
+        --------
+        CANMsgTable: returns self after adding message
+        """
+        if self.capacity is not None:
+            if (len(self.__message_table) < self.capacity
                     or (self.__message_table.get(frame.arb_id) is not None)):
                 self.__message_table[frame.arb_id] = frame
         else:
@@ -20,22 +46,54 @@ class CANMsgTable:
 
         return self
 
-    def __len__(self) -> int:
+    def __len__(self: CANMsgTable) -> int:
+        """
+        Overloaded len function
+
+        Returns
+        --------
+        int: Number of CANMsg records in table
+        """
         return len(self.__message_table)
 
-    def __str__(self) -> str:
+    def __str__(self: CANMsgTable) -> str:
+        """
+        Overloaded str function
+
+        Returns
+        --------
+        str: String representation of CANMsgTable
+        """
         attrs = []
         for k, v in self.__dict__.items():
             attrs += ['{}={}'.format(k, v)]
         return 'CANMsgTable {}\n\n'.format(', '.join(attrs))
 
-    def __getitem__(self, key: typing.Union[int, str]) -> CANMsg:
+    def __getitem__(self: CANMsgTable, key: typing.Union[int, str]) -> \
+            typing.Union[CANMsg, None]:
+        """
+        Overloaded getitem operator
+        Example: CANMsgTable[0x40]
+
+        Arguments
+        ----------
+        key: int or string representation of node COB-ID
+
+        Returns
+        --------
+        CANMsg: last message added for the provided COB-ID
+        None: None will be returned if no messages exist for provided COB-ID
+        """
         sub_key = int(key, 16) if type(key) is str else key
         return self.__message_table.get(sub_key)
 
-    def __iter__(self):
+    def __iter__(self: CANMsgTable) -> Iterator[CANMsg]:
+        """
+        Overloaded iter function
+
+        Returns
+        --------
+        Iterator[CANMsg]: iterator for contained messages
+        """
         return self.__message_table.__iter__()
 
-    @property
-    def capacity(self):
-        return self.__capacity
--- canopen_monitor/ui/pane.py
+++ canopen_monitor/ui/pane.py
@@ -6,19 +6,32 @@ from abc import ABC, abstractmethod
 
 
 class Pane(ABC):
-    def __init__(self: Pane, border: bool = True, color_pair: int = 4):
-        self._pad = curses.newpad(1, 1)
-        self.parent = curses.newwin(0, 0)
-        self.border = border
+    """
+    Abstract Pane Class, contains a PAD and a window.
+    """
 
-        # Pane state
-        self._needs_refresh = False
-        self._scroll_position_y = 0
-        self._scroll_position_x = 0
-        self.selected = False
+    def __init__(self: Pane, border: bool = True, color_pair: int = 4):
+        """
+        Abstract pane initialization
+
+        Arguments
+        ----------
+
+        border: boolean definition of whether or not to display border
+        color_pair: curses color pair to use (must be implemented prior)
+        """
+        self._pad = curses.newpad(1, 1)
         self._pad.scrollok(True)
+        self.parent = curses.newwin(0, 0)
+        self.__border = border
         self.v_width = 0
         self.v_height = 0
+
+        # Pane state
+        self.selected = False
+        self.needs_refresh = False
+        self.scroll_position_y = 0
+        self.scroll_position_x = 0
         self.scroll_limit_y = 0
         self.scroll_limit_x = 0
 
@@ -26,50 +39,144 @@ class Pane(ABC):
         self._style = curses.color_pair(color_pair)
 
     @abstractmethod
-    def draw(self: Pane):
-        if self._needs_refresh:
+    def draw(self: Pane) -> None:
+        """
+        Abstract draw method, must be overwritten in child class
+        draw should first resize the pad using: self._pad.resize()
+        then add content using: self._pad.addstr()
+        then refresh using: self._pad.refresh()
+
+        abstract method will clear and handle border
+
+        child class should also set _scroll_limit_x and _scroll_limit_y here
+        """
+        if self.needs_refresh:
             self.clear()
 
         self.parent.attron(self._style)
         self._pad.attron(self._style)
 
-        if self.border:
+        if self.__border:
             self.parent.box()
 
-    def clear(self):
+    def clear(self: Pane) -> None:
+        """
+        Clear all contents of pad and parent window
+        """
         self._pad.clear()
         self.parent.clear()
-        self._needs_refresh = False
+        self.needs_refresh = False
 
     @abstractmethod
-    def add(self: Pane, item: any):
+    def add(self: Pane, item: any) -> None:
+        """
+        Abstract add method, must be overwritten in child class
+        Child class should handle retrieving and storing added items
+        to be drawn by add method
+        """
         ...
 
-    def scroll_up(self, rate=1):
-        self._scroll_position_y -= rate
-        if self._scroll_position_y < 0:
-            self._scroll_position_y = 0
+    def scroll_up(self: Pane, rate: int = 1) -> bool:
+        """
+        Scroll pad upwards
 
-    def scroll_down(self, rate=1):
-        self._scroll_position_y += rate
-        if self._scroll_position_y > self.scroll_limit_y:
-            self._scroll_position_y = self.scroll_limit_y
+        Arguments
+        ----------
+        rate: number of lines to scroll up by
 
-    def scroll_left(self, rate=1):
-        self._scroll_position_x -= rate
-        if self._scroll_position_x < 0:
-            self._scroll_position_x = 0
+        Returns
+        --------
+        bool: Indication of whether a limit was reached. False indicates a
+        limit was reached and the pane cannot be scrolled further in that
+        direction
+        """
+        self.scroll_position_y -= rate
+        if self.scroll_position_y < 0:
+            self.scroll_position_y = 0
+            return False
+        return True
 
-    def scroll_right(self, rate=1):
-        self._scroll_position_x += rate
-        if self._scroll_position_x > self.scroll_limit_x:
-            self._scroll_position_x = self.scroll_limit_x
+    def scroll_down(self: Pane, rate: int = 1) -> None:
+        """
+        Scroll pad downwards
+
+        Note: scroll limit must be handled by child class
+
+        Arguments
+        ----------
+        rate: number of lines to scroll down by
+
+        Returns
+        --------
+        bool: Indication of whether a limit was reached. False indicates a
+        limit was reached and the pane cannot be scrolled further in that
+        """
+        self.scroll_position_y += rate
+        if self.scroll_position_y > self.scroll_limit_y:
+            self.scroll_position_y = self.scroll_limit_y
+            return False
+        return True
+
+    def scroll_left(self: Pane, rate: int = 1):
+        """
+        Scroll pad left
+
+        Arguments
+        ----------
+        rate: number of cols to scroll left by
+
+        Returns
+        --------
+        bool: Indication of whether a limit was reached. False indicates a
+        limit was reached and the pane cannot be scrolled further in that
+        """
+        self.scroll_position_x -= rate
+        if self.scroll_position_x < 0:
+            self.scroll_position_x = 0
+            return False
+        return True
+
+    def scroll_right(self: Pane, rate: int = 1):
+        """
+        Scroll pad right
+
+        Note: scroll limit must be handled by child class
+
+        Arguments
+        ----------
+        rate: number of cols to scroll right by
+
+        Returns
+        --------
+        bool: Indication of whether a limit was reached. False indicates a
+        limit was reached and the pane cannot be scrolled further in that
+        """
+        self.scroll_position_x += rate
+        if self.scroll_position_x > self.scroll_limit_x:
+            self.scroll_position_x = self.scroll_limit_x
+            return False
+        return True
 
 
-class CANMsgPane(Pane, ABC):
+class CANMsgPane(Pane):
+    """
+    Displays a Table of CAN messages inside of a pane
+    """
+
     def __init__(self: CANMsgPane, name: str, parser: CANOpenParser,
                  capacity: int = None, fields: dict = None, frame_types: list
                  = None):
+        """
+        CANMsgPane Initialization
+
+        Arguments
+        ----------
+        name: string title displayed on parent window
+        parser: CANOpenParser used to generated parsed messages
+        capacity: Maximum number of records to display in pane
+        fields: ordered dictionary of fields to display in output (layout.json)
+        frame_types: list of frame types to display in table (layout.json)
+        """
         super().__init__()
         if fields is None:
             fields = {}
@@ -77,9 +184,8 @@ class CANMsgPane(Pane, ABC):
         if frame_types is None:
             frame_types = []
 
-        self.name = name
+        self._name = name
         self._parser = parser
-        self.capacity = capacity
         self._cols = {}
         self.table = CANMsgTable(capacity=capacity)
 
@@ -93,7 +199,12 @@ class CANMsgPane(Pane, ABC):
         for ft in frame_types:
             self.frame_types.append(MessageType[ft])
 
-    def draw(self: CANMsgPane):
+    def draw(self: CANMsgPane) -> None:
+        """
+        Draw all records from the CANMsgTable to the Pane's Pad
+        and any relevent information to the Pane's parent window
+        Pane scrolling and refreshing are implemented here as well
+        """
         super().draw()
         height, width = self.parent.getmaxyx()
         y_offset, x_offset = self.parent.getbegyx()
@@ -108,9 +219,9 @@ class CANMsgPane(Pane, ABC):
         self._pad.resize(self.v_height - 1, self.v_width)
 
         # Update parent
-        out_of = '/{}'.format(self.capacity) \
-            if self.capacity is not None else ''
-        banner = '{} ({}{})'.format(self.name,
+        out_of = '/{}'.format(self.table.capacity) \
+            if self.table.capacity is not None else ''
+        banner = '{} ({}{})'.format(self._name,
                                     len(self.table),
                                     out_of)
 
@@ -126,13 +237,13 @@ class CANMsgPane(Pane, ABC):
             if self.v_height < 2:
                 self.v_height = 2
 
-            self.add_line(1, 1, "No fields added for this pane!", bold=True)
-            self.add_line(2, 1, "Add fields in "
-                                "~/.config/canopen-monitor/layout.json",
-                          bold=True)
+            self.__add_line(1, 1, "No fields added for this pane!", bold=True)
+            self.__add_line(2, 1, "Add fields in "
+                                  "~/.config/canopen-monitor/layout.json",
+                            bold=True)
 
         else:
-            self.draw_header()
+            self.__draw_header()
 
         for i, arb_id in enumerate(self.table):
             msg = self.table[arb_id]
@@ -149,20 +260,20 @@ class CANMsgPane(Pane, ABC):
 
                 line += value.ljust(col[1], ' ')
 
-            is_selected = self.selected and self._scroll_position_y == i
-            self.add_line(i + 1, 1, line, selected=is_selected)
+            is_selected = self.selected and self.scroll_position_y == i
+            self.__add_line(i + 1, 1, line, selected=is_selected)
 
         # Don't Scroll down until after scrolling past last item
-        if self._scroll_position_y < height - 3:
+        if self.scroll_position_y < height - 3:
             scroll_offset_y = 0
         else:
-            scroll_offset_y = self._scroll_position_y - (height - 4)
+            scroll_offset_y = self.scroll_position_y - (height - 4)
 
         # Don't allow for for pad to be seen past v_width
-        if self._scroll_position_x + width > self.v_width:
-            self._scroll_position_x = self.v_width - width
+        if self.scroll_position_x + width > self.v_width:
+            self.scroll_position_x = self.v_width - width
 
-        scroll_offset_x = self._scroll_position_x
+        scroll_offset_x = self.scroll_position_x
 
         self.parent.refresh()
         self._pad.refresh(scroll_offset_y,
@@ -172,10 +283,20 @@ class CANMsgPane(Pane, ABC):
                           y_offset + height - 2,
                           x_offset + width - 2)
 
-    def add_line(self, y: int, x: int, line: str = "", bold: bool = False,
-                 selected: bool = False) \
-            -> None:
+    def __add_line(self: CANMsgPane, y: int, x: int, line: str = "",
+                   bold: bool = False, selected: bool = False) -> None:
+        """
+        Add line of text to the Pane's pad
+        Handles resizing and pad attributes
 
+        Arguments
+        ----------
+        y: Pad's row to start writing to
+        x: Pad's col to start writing to
+        line: string of text to write
+        bold: boolean indicator of whether to bold line
+        selected: boolean indicator of whether to mark line selected
+        """
         # Widen pad when line length is larger than current v_width
         if len(line) + 2 > self.v_width:
             self.v_width = len(line) + 2
@@ -193,14 +314,25 @@ class CANMsgPane(Pane, ABC):
         if selected:
             self._pad.attroff(self._style | curses.A_REVERSE)
 
-    def draw_header(self) -> None:
+    def __draw_header(self: CANMsgPane) -> None:
+        """
+        Draw the table header to the top of the pane
+        """
         line = ""
         for col in self._cols:
             line += col.ljust(self._cols[col][1], ' ')
 
-        self.add_line(0, 1, line, bold=True)
+        self.__add_line(0, 1, line, bold=True)
 
-    def add(self: CANMsgPane, msg: CANMsg):
+    def add(self: CANMsgPane, msg: CANMsg) -> None:
+        """
+        Add provided message to the can msg table
+        and update column widths as required
+
+        Arguments
+        ----------
+        msg: CanMsg to be added
+        """
         super().add(msg)
         msg.parsed_msg = self._parser.parse(msg)[0]
         self.table += msg
@@ -214,5 +346,15 @@ class CANMsgPane(Pane, ABC):
 
             col[1] = len(value) + 2 if len(value) + 2 > col[1] else col[1]
 
-    def has_frame_type(self, frame):
+    def has_frame_type(self: CANMsgPane, frame: CANMsg) -> bool:
+        """
+        Determine if CANMsg type is handled by this frame
+        Arguments
+        ----------
+        frame: CANMsg to check type of
+
+        Returns
+        --------
+        bool: Indicator if this pane accepts the message type of the provided message
+        """
         return frame.message_type in self.frame_types
