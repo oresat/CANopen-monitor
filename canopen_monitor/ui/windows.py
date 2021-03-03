474d32ee995276cea09b5f1a6d0e524b1bdfb609
nhanlt81 <ntl2@pdx.edu>
2021-03-03 11:57:21 -0800

    Update windows.py

--- canopen_monitor/ui/windows.py
+++ canopen_monitor/ui/windows.py
@@ -4,6 +4,7 @@ from .pane import Pane
 
 
 class PopupWindow(Pane):
+
     def __init__(self: PopupWindow,
                  parent: any,
                  header: str = 'Alert',
@@ -16,10 +17,7 @@ class PopupWindow(Pane):
                          y=10,
                          x=10)
         # Pop-up window properties
-        self.header = header
-        self.content = content
-        self.footer = footer
-        self.enabled = False
+        self.setWindowProperties(header, content, footer)
 
         # Parent window dimensions (Usually should be STDOUT directly)
         p_height, p_width = self.parent.getmaxyx()
@@ -28,19 +26,31 @@ class PopupWindow(Pane):
         self.content = self.break_lines(int(2 * p_width / 3), self.content)
 
         # UI dimensions
-        p_height, p_width = self.parent.getmaxyx()
-        self.v_height = (len(self.content)) + 2
-        width = len(self.header) + 2
-        if(len(self.content) > 0):
-            width = max(width, max(list(map(lambda x: len(x), self.content))))
-        self.v_width = width + 4
-        self.y = int(((p_height + self.v_height) / 2) - self.v_height)
-        self.x = int(((p_width + self.v_width) / 2) - self.v_width)
+        p_height, p_width = self.parent.getmaxyx() # Would we consider to reuse it?
+        self.setUIDimension(p_height, p_width)
 
         # UI properties
         self.style = (style or curses.color_pair(0))
         self._pad.attron(self.style)
 
+    # Set UI Dimension (x,y) by giving parent height and width
+    def setUIDimension(self, p_height, p_width):
+        self.v_height = (len(self.content)) + 2
+        width = len(self.header) + 2
+        if (len(self.content) > 0):
+            width = max(width, max(list(map(lambda x: len(x), self.content))))
+        self.v_width = width + 4
+        self.y = int(((p_height + self.v_height) / 2) - self.v_height)
+        self.x = int(((p_width + self.v_width) / 2) - self.v_width)
+
+    # Set default window properties
+    def setWindowProperties(self:PopupWindow, header, content, footer):
+        self.header = header
+        self.content = content
+        self.footer = footer
+        self.enabled = False
+
+    # Take a content and break it to the lines
     def break_lines(self: PopupWindow,
                     max_width: int,
                     content: [str]) -> [str]:
@@ -62,13 +72,16 @@ class PopupWindow(Pane):
                 content.insert(i + 1, line[mid:])
         return content
 
+
     def toggle(self: PopupWindow) -> bool:
         self.enabled = not self.enabled
         return self.enabled
 
+    # Add the header to the window
     def __draw_header(self: PopupWindow) -> None:
         self.add_line(0, 1, self.header, underline=True)
 
+    # Add the footer to the window
     def __draw__footer(self: PopupWindow) -> None:
         f_width = len(self.footer) + 2
         self.add_line(self.v_height - 1,
@@ -76,15 +89,19 @@ class PopupWindow(Pane):
                       self.footer,
                       underline=True)
 
+    # Read each line of the content and add to the window
+    def __draw_content(self):
+        for i, line in enumerate(self.content):
+            self.add_line(1 + i, 2, line)
+
+    # Draw the Content to the Window
     def draw(self: PopupWindow) -> None:
         if(self.enabled):
             super().resize(self.v_height, self.v_width)
             super().draw()
+            # start to draw
             self.__draw_header()
-
-            for i, line in enumerate(self.content):
-                self.add_line(1 + i, 2, line)
-
+            self.__draw_content()
             self.__draw__footer()
             super().refresh()
         else:
