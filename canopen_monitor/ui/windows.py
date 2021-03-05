b24ceb326a2688323a860c5c33ae11e00c5999c0
nhanlt81 <ntl2@pdx.edu>
2021-03-04 18:14:16 -0800

    Update windows.py
    Update the comments for individual function to be within and use three " marks

--- canopen_monitor/ui/windows.py
+++ canopen_monitor/ui/windows.py
@@ -16,6 +16,7 @@ class PopupWindow(Pane):
                          width=1,
                          y=10,
                          x=10)
+        """Set an init to Popup window"""
         # Pop-up window properties
         self.setWindowProperties(header, content, footer)
 
@@ -26,15 +27,16 @@ class PopupWindow(Pane):
         self.content = self.break_lines(int(2 * p_width / 3), self.content)
 
         # UI dimensions
-        p_height, p_width = self.parent.getmaxyx() # Would we consider to reuse it?
         self.setUIDimension(p_height, p_width)
 
         # UI properties
         self.style = (style or curses.color_pair(0))
         self._pad.attron(self.style)
 
-    # Set UI Dimension (x,y) by giving parent height and width
+
     def setUIDimension(self, p_height, p_width):
+        """Set UI Dimension (x,y) by giving parent
+        height and width"""
         self.v_height = (len(self.content)) + 2
         width = len(self.header) + 2
         if (len(self.content) > 0):
@@ -43,14 +45,15 @@ class PopupWindow(Pane):
         self.y = int(((p_height + self.v_height) / 2) - self.v_height)
         self.x = int(((p_width + self.v_width) / 2) - self.v_width)
 
-    # Set default window properties
+
     def setWindowProperties(self:PopupWindow, header, content, footer):
+        """Set default window properties"""
         self.header = header
         self.content = content
         self.footer = footer
         self.enabled = False
 
-    # Take a content and break it to the lines
+
     def break_lines(self: PopupWindow,
                     max_width: int,
                     content: [str]) -> [str]:
@@ -59,47 +62,54 @@ class PopupWindow(Pane):
             length = len(line)
             mid = int(length / 2)
 
-            if(length >= max_width):
-                # Break the line at the next available space
-                for j, c in enumerate(line[mid - 1:]):
-                    if(c == ' '):
-                        mid += j
-                        break
-
-                # Apply the line break to the content array
-                content.pop(i)
-                content.insert(i, line[:mid - 1])
-                content.insert(i + 1, line[mid:])
+            self.determine_to_break_content(content, i, length, line, max_width, mid)
         return content
 
+    def determine_to_break_content(self, content, i, length, line, max_width, mid):
+        if (length >= max_width):
+            # Break the line at the next available space
+            for j, c in enumerate(line[mid - 1:]):
+                if (c == ' '):
+                    mid += j
+                    break
+            self.apply_line_to_content_array(content, i, line, mid)
+
+
+    def apply_line_to_content_array(self, content, i, line, mid):
+        """Apply the line break to the content array"""
+        content.pop(i)
+        content.insert(i, line[:mid - 1])
+        content.insert(i + 1, line[mid:])
 
     def toggle(self: PopupWindow) -> bool:
         self.enabled = not self.enabled
         return self.enabled
 
-    # Add the header to the window
+
     def __draw_header(self: PopupWindow) -> None:
+        """Add the header line to the window"""
         self.add_line(0, 1, self.header, underline=True)
 
-    # Add the footer to the window
+
     def __draw__footer(self: PopupWindow) -> None:
+        """Add the footer to the window"""
         f_width = len(self.footer) + 2
         self.add_line(self.v_height - 1,
                       self.v_width - f_width,
                       self.footer,
                       underline=True)
 
-    # Read each line of the content and add to the window
+
     def __draw_content(self):
+        """Read each line of the content and add to the window"""
         for i, line in enumerate(self.content):
             self.add_line(1 + i, 2, line)
 
-    # Draw the Content to the Window
+
     def draw(self: PopupWindow) -> None:
         if(self.enabled):
             super().resize(self.v_height, self.v_width)
             super().draw()
-            # start to draw
             self.__draw_header()
             self.__draw_content()
             self.__draw__footer()
