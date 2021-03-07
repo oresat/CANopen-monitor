9da66ac9b329a881c77ffd0d0ecb6b2084d0789b
thaotran <thao1.tran@intel.com>
2021-03-06 22:52:57 -0800

    added magic number for state byte index for readability

--- canopen_monitor/parse/hb.py
+++ canopen_monitor/parse/hb.py
@@ -9,20 +9,24 @@ def parse(cob_id: int, data: list, eds_config: EDS):
 
     Arguments
     ---------
-    @:param: data: a byte string containing the heartbeat message
+    @:param: data: a byte string containing the heartbeat message,
+                   byte 0 is the heartbeat state info.
 
     Returns
     -------
     `str`: The parsed message
     """
+    STATE_BYTE_IDX = 0
     states = {
         0x00: "Boot-up",
         0x04: "Stopped",
         0x05: "Operational",
         0x7F: "Pre-operational"
     }
+
     node_id = MessageType.cob_to_node(MessageType.HEARTBEAT, cob_id)
-    if len(data) < 1 or data[0] not in states:
+    hb_state = data[STATE_BYTE_IDX]
+    if len(data) < 1 or hb_state not in states:
         raise FailedValidationError(data, node_id, cob_id, __name__,
                                     "Invalid heartbeat state detected")
-    return states.get(data[0])
+    return states.get(hb_state)
