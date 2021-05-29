ccc86d9635b46ce27f06b2109315172a77a0b215
Brian ONeill <Boneill3@users.noreply.github.com>
2021-05-29 15:28:50 -0700

    Version and validate config file close #39

--- canopen_monitor/meta.py
+++ canopen_monitor/meta.py
@@ -1,6 +1,12 @@
+from __future__ import annotations
+from abc import ABC
+
+import os
+
 from .can import MagicCANBus
 from os import path
 import json
+from json import JSONDecodeError
 
 
 class Meta:
@@ -9,24 +15,107 @@ class Meta:
         self.cache_dir = cache_dir
         self.interfaces_file = self.config_dir + '/interfaces.json'
         self.nodes_file = self.config_dir + '/nodes.json'
+        self.feature_file = self.config_dir + '/features.json'
 
-    def save_devices(self, mcb: MagicCANBus) -> bool:
-        output = {'interfaces': mcb.interface_list}
-        with open(self.interfaces_file, "w") as f:
-            json.dump(output, f)
-            f.truncate()
+    def save_interfaces(self, mcb: MagicCANBus) -> None:
+        interfaceConfig = InterfaceConfig()
+        interfaceConfig.interfaces = mcb.interface_list
+        write_config(self.interfaces_file, interfaceConfig)
 
-    def load_devices(self, interface_args: [str]) -> [str]:
-        if not path.isfile(self.interfaces_file):
-            return interface_args
-
-        with open(self.interfaces_file, "r") as f:
-            interface_config = json.load(f)
-            for interface in interface_config['interfaces']:
-                if interface not in interface_args:
-                    interface_args.append(interface)
+    def load_interfaces(self, interface_args: [str]) -> [str]:
+        interfaceConfig = InterfaceConfig()
+        load_config(self.interfaces_file, interfaceConfig)
+        for interface in interfaceConfig.interfaces:
+            if interface not in interface_args:
+                interface_args.append(interface)
 
         return interface_args
 
+    def load_features(self) -> FeatureConfig:
+        features = FeatureConfig()
+        load_config(self.feature_file, features)
+        return features
+
     def load_node_overrides(self) -> dict:
         pass
+
+
+def write_config(filename: str, config: Config) -> None:
+    output = config.__dict__
+    with open(filename, "w") as f:
+        json.dump(output, f, indent=4)
+        f.truncate()
+
+
+def load_config(filename: str, config: Config) -> None:
+    try:
+        if not path.isfile(filename):
+            return write_config(filename, config)
+
+        with open(filename, "r") as f:
+            json_data = json.load(f)
+
+        parsed_version = str(json_data.get('version', '1.0')).split('.')
+
+        # Major version mismatch or invalid version indicates a breaking change
+        if len(parsed_version) != 2 or not parsed_version[0].isnumeric() or\
+                not parsed_version[1].isnumeric() or int(parsed_version[0]) != config.MAJOR:
+            return overwrite_config(filename, config)
+
+        return config.load(json_data)
+
+    except (JSONDecodeError, OSError, IOError):
+        return overwrite_config(filename, config)
+
+
+def overwrite_config(filename: str, config: Config) -> None:
+    if path.isfile(filename):
+        backup_filename = filename + ".bak"
+        count = 1
+        while path.isfile(backup_filename):
+            backup_filename = filename + f"-{count}.bak"
+            count += 1
+
+        os.rename(filename, backup_filename)
+
+    return write_config(filename, config)
+
+
+class Config(ABC):
+    MAJOR = 1
+    MINOR = 0
+
+    def __init__(self, major: int, minor: int):
+        self.version = f"{major}.{minor}"
+
+    def load(self, data: dict) -> None:
+        self.version = data.get('version', self.version)
+
+
+class FeatureConfig(Config):
+    MAJOR = 1
+    MINOR = 0
+
+    def __init__(self):
+        super().__init__(self.MAJOR, self.MINOR)
+        self.ecss_time = False
+
+    def load(self, data: dict) -> None:
+        super().load(data)
+        self.ecss_time = data.get('ecss_time', self.ecss_time)
+
+
+class InterfaceConfig(Config):
+    MAJOR = 1
+    MINOR = 0
+
+    def __init__(self):
+        super().__init__(self.MAJOR, self.MINOR)
+        self.interfaces = []
+
+    def load(self, data: dict) -> None:
+        super().load(data)
+
+        loaded_interfaces = data.get('interfaces', self.interfaces)
+        for interface in loaded_interfaces:
+            self.interfaces.append(interface)
--- tests/spec_meta.py
+++ tests/spec_meta.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import mock_open, patch, MagicMock, call
-from canopen_monitor.meta import Meta
+from canopen_monitor.meta import Meta, load_config, Config, InterfaceConfig, FeatureConfig
 
 
 class TestMeta(unittest.TestCase):
@@ -11,58 +11,78 @@ class TestMeta(unittest.TestCase):
         self.meta = Meta("config_dir", "cache_dir")
         self.mcb = MagicMock()
 
-    def test_save_single_device(self):
+    def test_save_single_interface(self):
         """
-        Test Save a single Device
+        Test Save a single interface
         """
         self.mcb.interface_list = ["vcan0"]
         with patch('builtins.open', mock_open()) as m:
-            self.meta.save_devices(self.mcb)
+            self.meta.save_interfaces(self.mcb)
 
         m.assert_called_once_with('config_dir/interfaces.json', 'w')
         calls = [call('{'),
+                 call('\n    '),
+                 call('"version"'),
+                 call(': '),
+                 call(f'"{InterfaceConfig.MAJOR}.{InterfaceConfig.MINOR}"'),
+                 call(',\n    '),
                  call('"interfaces"'),
                  call(': '),
-                 call('["vcan0"'),
+                 call('[\n        "vcan0"'),
+                 call('\n    '),
                  call(']'),
+                 call('\n'),
                  call('}')]
 
         self.assertEqual(calls, m().write.mock_calls,
                          "json file not written out correctly")
 
-    def test_save_multiple_device(self):
+    def test_save_multiple_interface(self):
         """
-        Test Save Multiple Devices
+        Test Save Multiple interfaces
         """
         self.mcb.interface_list = ["vcan0", "vcan1"]
         with patch('builtins.open', mock_open()) as m:
-            self.meta.save_devices(self.mcb)
+            self.meta.save_interfaces(self.mcb)
 
         m.assert_called_once_with('config_dir/interfaces.json', 'w')
         calls = [call('{'),
+                 call('\n    '),
+                 call('"version"'),
+                 call(': '),
+                 call(f'"{InterfaceConfig.MAJOR}.{InterfaceConfig.MINOR}"'),
+                 call(',\n    '),
                  call('"interfaces"'),
                  call(': '),
-                 call('["vcan0"'),
-                 call(', "vcan1"'),
+                 call('[\n        "vcan0"'),
+                 call(',\n        "vcan1"'),
+                 call('\n    '),
                  call(']'),
+                 call('\n'),
                  call('}')]
 
         self.assertEqual(calls, m().write.mock_calls,
                          "json file not written out correctly")
 
-    def test_save_no_device(self):
+    def test_save_no_interface(self):
         """
-        Test Save No Devices
+        Test Save No interfaces
         """
         self.mcb.interface_list = []
         with patch('builtins.open', mock_open()) as m:
-            self.meta.save_devices(self.mcb)
+            self.meta.save_interfaces(self.mcb)
 
         m.assert_called_once_with('config_dir/interfaces.json', 'w')
         calls = [call('{'),
+                 call('\n    '),
+                 call('"version"'),
+                 call(': '),
+                 call(f'"{InterfaceConfig.MAJOR}.{InterfaceConfig.MINOR}"'),
+                 call(',\n    '),
                  call('"interfaces"'),
                  call(': '),
                  call('[]'),
+                 call('\n'),
                  call('}')]
 
         self.assertEqual(calls, m().write.mock_calls,
@@ -70,59 +90,122 @@ class TestMeta(unittest.TestCase):
 
         m().truncate.assert_called_once()
 
-    def test_load_single_device(self):
+    def test_load_single_interface(self):
         """
-        Test load a single Device
+        Test load a single interface
         """
 
         json = '{"interfaces": ["vcan0"]}'
         with patch('builtins.open', mock_open(read_data=json)) as m:
             with patch('os.path.isfile', return_value=True) as m_os:
-                devices = self.meta.load_devices([])
+                interfaces = self.meta.load_interfaces([])
 
         m.assert_called_once_with('config_dir/interfaces.json', 'r')
         m_os.assert_called_once_with('config_dir/interfaces.json')
-        self.assertEqual(["vcan0"], devices,
-                         "Devices not loaded correctly")
+        self.assertEqual(["vcan0"], interfaces,
+                         "interfaces not loaded correctly")
 
-    def test_load_multiple_devices(self):
+    def test_load_multiple_interfaces(self):
         """
-        Test load of multiple Device
+        Test load of multiple interface
         """
 
         json = '{"interfaces": ["vcan0", "vcan1"]}'
         with patch('builtins.open', mock_open(read_data=json)) as m:
             with patch('os.path.isfile', return_value=True) as m_os:
-                devices = self.meta.load_devices([])
+                interfaces = self.meta.load_interfaces([])
 
         m.assert_called_once_with('config_dir/interfaces.json', 'r')
         m_os.assert_called_once_with('config_dir/interfaces.json')
-        self.assertEqual(["vcan0", "vcan1"], devices,
-                         "Devices not loaded correctly")
+        self.assertEqual(["vcan0", "vcan1"], interfaces,
+                         "interfaces not loaded correctly")
 
-    def test_load_no_devices(self):
+    def test_load_no_interfaces(self):
         """
-        Test load of no Devices
+        Test load of no interfaces
         """
 
         json = '{"interfaces": []}'
         with patch('builtins.open', mock_open(read_data=json)) as m:
             with patch('os.path.isfile', return_value=True) as m_os:
-                devices = self.meta.load_devices([])
+                interfaces = self.meta.load_interfaces([])
 
         m.assert_called_once_with('config_dir/interfaces.json', 'r')
         m_os.assert_called_once_with('config_dir/interfaces.json')
-        self.assertEqual([], devices,
-                         "Devices not loaded correctly")
+        self.assertEqual([], interfaces,
+                         "interfaces not loaded correctly")
 
-    def test_load_no_file(self):
+    def test_load_no_interfaces_file(self):
         """
         Test load with no existing file
         """
 
-        with patch('os.path.isfile', return_value=False) as m_os:
-            devices = self.meta.load_devices([])
+        with patch('builtins.open', mock_open()) as m:
+            with patch('os.path.isfile', return_value=False) as m_os:
+                interfaces = self.meta.load_interfaces([])
 
-        m_os.assert_called_once_with('config_dir/interfaces.json')
-        self.assertEqual([], devices,
-                         "Devices not loaded correctly")
+            m_os.assert_called_once_with('config_dir/interfaces.json')
+            self.assertEqual([], interfaces,
+                             "interfaces not loaded correctly")
+
+    def test_load_features(self):
+        """
+        Test loading features from feature file
+        :return:
+        """
+        json = f'{{"major": {FeatureConfig.MAJOR}, "minor": {FeatureConfig.MINOR}, "ecss_time": true }}'
+        with patch('builtins.open', mock_open(read_data=json)) as m:
+            with patch('os.path.isfile', return_value=True) as m_os:
+                features = self.meta.load_features()
+
+        m.assert_called_once_with('config_dir/features.json', 'r')
+        m_os.assert_called_once_with('config_dir/features.json')
+        self.assertEqual(True, features.ecss_time,
+                         "features not loaded correctly")
+
+    def test_replace_breaking_config(self):
+        """
+        Test loading an existing file with a breaking version
+        Should create a backup and save a new file with defaults
+        :return:
+        """
+        def isfile(filename):
+            if filename == "filename":
+                return True
+            else:
+                return False
+
+        isfileMock = MagicMock(side_effect=isfile)
+        with patch('builtins.open', mock_open()) as m:
+            with patch('os.path.isfile', isfileMock) as m_os:
+                with patch('os.rename') as m_os_rename:
+                    config = Config(0, 0)
+                    load_config("filename", config)
+
+        m_os.assert_has_calls([call("filename"), call("filename.bak")])
+        m_os_rename.assert_called_once_with('filename', 'filename.bak')
+        m.assert_called_with('filename', 'w')
+
+    def test_replace_breaking_config_with_existing_bak(self):
+        """
+        Test loading an existing file with a breaking version
+        when an existing .bak file exists
+        :return:
+        """
+
+        def isfile(filename):
+            if filename == "filename" or filename == "filename.bak" or filename == "filename-1.bak":
+                return True
+            else:
+                return False
+
+        isfileMock = MagicMock(side_effect=isfile)
+        with patch('builtins.open', mock_open()) as m:
+            with patch('os.path.isfile', isfileMock) as m_os:
+                with patch('os.rename') as m_os_rename:
+                    config = Config(0, 0)
+                    load_config("filename", config)
+
+        m_os.assert_has_calls([call("filename"), call("filename.bak"), call("filename-1.bak")])
+        m_os_rename.assert_called_once_with('filename', 'filename-2.bak')
+        m.assert_called_with('filename', 'w')
